b84b0cec2b75f3cad47828fa7ea4a544bc439512
Vick Scarlet <scarlet_vick@Outlook.com>
2018-01-08 02:34:34 +0800

    Initial commit

---
+++ DMZJ漫画全屏自适应.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         DMZJ漫画全屏自适应
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  try to take over the world!
+// @author       VickScarlet
+// @match        http://manhua.dmzj.com/*shtml*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  var div = document.getElementsByTagName("div");
+  for(var i=0;i<div.length;i++){
+      div[i].style.display = "none";
+  }
+  document.getElementById("center_box").style.position = "absolute";
+  document.getElementById("center_box").style.display = "flex";
+  var css = "";
+  css += [
+      "div {",
+      "    display: none;",
+      "    height: 0px;",
+      "}",
+      "#center_box {",
+      "    position: absolute;",
+      "    display: -webkit-flex;",
+      "    display: flex;",
+      "    -webkit-align-items: center;",
+      "    align-items: center;",
+      "    -webkit-justify-content: center;",
+      "    justify-content: center;",
+      "    margin: 0px;",
+      "    padding: 0px;",
+      "    top: 0%;",
+      "    left: 0%;",
+      "    width: 100%;",
+      "    height: 100%;",
+      "    background-color: black;",
+      "    z-index: 1;",
+      "}",
+      "#center_box > img {",
+      "    width: auto;",
+      "    height: 100%;",
+      "    left: 0;",
+      "    top: 0;",
+      "    border: 0px;",
+      "    margin: auto;",
+      "    padding: 0;",
+      "}"
+  ].join("\n");
+  if (typeof GM_addStyle != "undefined") {
+      GM_addStyle(css);
+  } else if (typeof PRO_addStyle != "undefined") {
+      PRO_addStyle(css);
+  } else if (typeof addStyle != "undefined") {
+      addStyle(css);
+  } else {
+      var node = document.createElement("style");
+      node.type = "text/css";
+      node.appendChild(document.createTextNode(css));
+      var heads = document.getElementsByTagName("head");
+      if (heads.length > 0) {
+          heads[0].appendChild(node);
+      } else {
+          // no head yet, stick it whereever
+          document.documentElement.appendChild(node);
+      }
+  }
+})();
