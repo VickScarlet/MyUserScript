3276bc56addfcdf02fc35f5587aa5745d4671822
Vick Scarlet <scarlet_vick@outlook.com>
2018-06-07 06:55:43 +0800

    add shortcut key

--- DMZJ漫画全屏自适应.js
+++ DMZJ漫画全屏自适应.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DMZJ漫画全屏自适应
 // @namespace    http://tampermonkey.net/
-// @version      0.0.3
+// @version      0.0.4
 // @description  动漫之家漫画全屏自适应
 // @author       VickScarlet
 // @include      /^(http)s?:\/\/(manhua\.dmzj\.com)\/.*\/[0-9]+.shtml.*
@@ -9,68 +9,100 @@
 // @grant        none
 // ==/UserScript==
 
-(function () {
-    'use strict';
-    var div = document.getElementsByTagName("div");
-    for (var i = 0; i < div.length; i++) {
-        if (div[i].className != "comic_wraCon autoHeight") {
-            div[i].style.display = "none";
-        } else {
-            div[i].id = "center_box";
-            div[i].className = "comic_wraCon";
-        }
+function initial(){
+    init_view();
+    init_key();
+    init_style();
+}
+
+function init_view(){
+    $("body").children().addClass("nodisplay");
+    let mode = $("#qiehuan_txt").text();
+    let html = `
+    <div id="scarletshow">
+        <div id="scarletshowbg">
+        </div>
+        <div id="scarletshowcontent">
+            <a id="scarlet_prevc">上一章</a>
+            <a id="scarlet_nextc">下一章</a>
+            <img id="imgcontent" index="0"/>
+        </div>
+    </div>`;
+    if(mode=="切换到上下滚动阅读"){
+        $("#qiehuan_txt").click();
+    }else{
+        $("body").append(html);
+        let count = arr_pages.length;
+        $("#imgcontent").attr('src',img_prefix+arr_pages[0]);
+        $("#scarlet_prevc").click(function (e) {
+            let prevc = $("#prev_chapter");
+            if(prevc.length > 0){ prevc[0].click(); }
+            else alert("没有前一章了");
+        });
+        $("#scarlet_nextc").click(function (e) {
+            let nextc = $("#next_chapter");
+            if(nextc.length > 0){ nextc[0].click(); }
+            else alert("没有后一章了");
+        });
     }
-    document.getElementById("center_box").style.position = "absolute";
-    document.getElementById("center_box").style.display = "flex";
-    var css = "";
-    css += [
-        "div {",
-        "    display: none;",
-        "    height: 0px;",
-        "}",
-        "#center_box {",
-        "    position: absolute;",
-        "    display: -webkit-flex;",
-        "    display: flex;",
-        "    -webkit-align-items: center;",
-        "    align-items: center;",
-        "    -webkit-justify-content: center;",
-        "    justify-content: center;",
-        "    margin: 0px;",
-        "    padding: 0px;",
-        "    top: 0%;",
-        "    left: 0%;",
-        "    width: 100%;",
-        "    height: 100%;",
-        "    background-color: black;",
-        "    z-index: 1;",
-        "}",
-        "#center_box > img {",
-        "    width: auto;",
-        "    height: 100%;",
-        "    left: 0;",
-        "    top: 0;",
-        "    border: 0px;",
-        "    margin: auto;",
-        "    padding: 0;",
-        "}"
-    ].join("\n");
-    if (typeof GM_addStyle != "undefined") {
-        GM_addStyle(css);
-    } else if (typeof PRO_addStyle != "undefined") {
-        PRO_addStyle(css);
-    } else if (typeof addStyle != "undefined") {
-        addStyle(css);
-    } else {
-        var node = document.createElement("style");
-        node.type = "text/css";
-        node.appendChild(document.createTextNode(css));
-        var heads = document.getElementsByTagName("head");
-        if (heads.length > 0) {
-            heads[0].appendChild(node);
-        } else {
-            // no head yet, stick it whereever
-            document.documentElement.appendChild(node);
+}
+
+function init_key(){
+    let curr = parseInt($("#imgcontent").attr("index"));
+    let prev = curr - 1;
+    let next = curr + 1;
+    let count = arr_pages.length;
+    console.log("count"+count);
+    $('body').keyup(function(event) {
+        if (document.activeElement.localName != "input" && document.activeElement.localName != "select" ) {
+            var value = event.keyCode;
+            if (value == 88) {//x
+                let curr = parseInt($("#imgcontent").attr("index"));
+                let prev = curr - 1;
+                if(prev<0){
+                    var prevc = $("#prev_chapter");
+                    if(prevc.length > 0){ prevc[0].click(); }
+                    else alert("已经是最前了");
+                }else{
+                    $("#imgcontent").attr('index',prev);
+                    $("#imgcontent").attr('src',img_prefix+arr_pages[prev]);
+                }
+            }else if(value == 67){//c
+                let curr = parseInt($("#imgcontent").attr("index"));
+                let next = curr + 1;
+                console.log("next"+next);
+                if(next+1>count){
+                    var nextc = $("#next_chapter");
+                    if(nextc.length > 0){ nextc[0].click(); }
+                    else alert("已经是最后了");
+                }else{
+                    $("#imgcontent").attr('index',next);
+                    $("#imgcontent").attr('src',img_prefix+arr_pages[next]);
+                }
+            }else if(value == 90){//z
+                let prevc = $("#prev_chapter");
+                if(prevc.length > 0){ prevc[0].click(); }
+                else alert("没有前一章了");
+            }else if(value == 86){//v
+                let nextc = $("#next_chapter");
+                if(nextc.length > 0){ nextc[0].click(); }
+                else alert("没有后一章了");
+            }else if(value == 81){//q
+                $("body").children(".nodisplay").removeClass("nodisplay");
+                $("#scarletshow").attr("class","nodisplay");
+            }else if(value == 65){//a
+                $("body").children().addClass("nodisplay");
+                $("#scarletshow").attr("class","");
+            }
         }
-    }
-})();
+    });
+}
+
+function init_style(){
+    let style = `<style id="scarletview">#scarletshow{position:fixed;width:100%;height:100%;top:0}#scarletshowbg{background:black;width:100%;height:100%;opacity:0.5;}#scarletshowcontent{position:absolute;width:100%;height:100%;top:0;}#scarletshowcontent>a{z-index:10000;}#imgcontent{position:absolute;max-width:100%;max-height:100%;top:50%;left:50%;transform:translateY(-50%)translateX(-50%);}.nodisplay{display:none;}</style>`;
+    $("head").append(style);
+}
+
+$(document).ready(function() {
+    initial();
+});
