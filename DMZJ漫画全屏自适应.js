f9e32263c51930d41a12eaac5d9e91bc34ab2e71
Vick Scarlet <scarlet_vick@outlook.com>
2018-07-03 20:41:45 +0800

    修复国漫界面无法使用问题

--- DMZJ漫画全屏自适应.js
+++ DMZJ漫画全屏自适应.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DMZJ漫画全屏自适应
 // @namespace    http://tampermonkey.net/
-// @version      0.0.4
+// @version      0.0.5
 // @description  动漫之家漫画全屏自适应
 // @author       VickScarlet
 // @include      /^(http)s?:\/\/(manhua\.dmzj\.com)\/.*\/[0-9]+.shtml.*
@@ -11,44 +11,35 @@
 /*jshint esversion: 6 */
 
 function initial(){
+    set_mode();
     init_view();
     init_key();
     init_style();
 }
 
+
 function init_view(){
+    $("#app_manhua").attr("style","width:800px; height:120px; padding:20px; background:#fff; border:1px solid #ccc; margin:20px auto");
     $("body").children().addClass("nodisplay");
-    let mode = $("#qiehuan_txt").text();
     let html = `
     <div id="scarletshow">
         <div id="scarletshowbg">
         </div>
         <div id="scarletshowcontent">
-            <a id="scarlet_prevc">上一章</a>
-            <a id="scarlet_nextc">下一章</a>
             <img id="imgcontent" index="0"/>
         </div>
     </div>`;
-    if(mode=="切换到上下滚动阅读"){
-        $("#qiehuan_txt").click();
-    }else{
-        $("body").append(html);
-        let count = arr_pages.length;
-        $("#imgcontent").attr('src',img_prefix+arr_pages[0]);
-        $("#scarlet_prevc").click(function (e) {
-            let prevc = $("#prev_chapter");
-            if(prevc.length > 0){ prevc[0].click(); }
-            else alert("没有前一章了");
-        });
-        $("#scarlet_nextc").click(function (e) {
-            let nextc = $("#next_chapter");
-            if(nextc.length > 0){ nextc[0].click(); }
-            else alert("没有后一章了");
-        });
+    $("body").append(html);
+    if(typeof(arr_pages) == "undefined"){
+        arr_pages = picArry;
     }
+    $("#imgcontent").attr('src',img_prefix+arr_pages[0]);
 }
 
 function init_key(){
+    if(typeof(arr_pages) == "undefined"){
+        arr_pages = picArry;
+    }
     let count = arr_pages.length;
     $('body').keyup(function(event) {
         if (document.activeElement.localName != "input" && document.activeElement.localName != "select" ) {
@@ -57,7 +48,7 @@ function init_key(){
                 let curr = parseInt($("#imgcontent").attr("index"));
                 let prev = curr - 1;
                 if(prev<0){
-                    let prevc = $("#prev_chapter");
+                    let prevc = $("a.btm_chapter_btn.fl");
                     if(prevc.length > 0){ prevc[0].click(); }
                     else alert("已经是最前了");
                 }else{
@@ -68,7 +59,7 @@ function init_key(){
                 let curr = parseInt($("#imgcontent").attr("index"));
                 let next = curr + 1;
                 if(next+1>count){
-                    let nextc = $("#next_chapter");
+                    let nextc = $("a.btm_chapter_btn.fr");
                     if(nextc.length > 0){ nextc[0].click(); }
                     else alert("已经是最后了");
                 }else{
@@ -76,11 +67,11 @@ function init_key(){
                     $("#imgcontent").attr('src',img_prefix+arr_pages[next]);
                 }
             }else if(value == 90){//z
-                let prevc = $("#prev_chapter");
+                let prevc = $("a.btm_chapter_btn.fl");
                 if(prevc.length > 0){ prevc[0].click(); }
                 else alert("没有前一章了");
             }else if(value == 86){//v
-                let nextc = $("#next_chapter");
+                let nextc = $("a.btm_chapter_btn.fr");
                 if(nextc.length > 0){ nextc[0].click(); }
                 else alert("没有后一章了");
             }else if(value == 81){//q
@@ -92,6 +83,19 @@ function init_key(){
             }
         }
     });
+}
+
+function set_mode(){
+    if(location.hostname=="manhua.dmzj.com"){
+        if($("#qiehuan_txt").text()=="切换到上下滚动阅读"){
+            $("#qiehuan_txt").click();
+        }
+    } else {
+        if($("input[name=mode]:checked").val()=="0"){
+            $("input[name=mode]:checked").val("1");
+            reset();
+        }
+    }
 }
 
 function init_style(){
